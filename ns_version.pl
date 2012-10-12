d2315fb9e6035d9b34442bd4336344353be08fef
Anton Belousov <erbol.turburgaev@gmail.com>
2012-10-13 02:40:40 +0400

    NS version now accepts domains only.

--- ns_version.pl
+++ ns_version.pl
@@ -1,6 +1,7 @@
 #!perl
 
 use strict;
+use Net::DNS;
 
 # nslookup -type=txt -cl=chaos version.bind. muenchow.ch
 unless ( @ARGV ) { print q[Error: argument list is empty], "\n"; exit(0); };
@@ -11,26 +12,59 @@ my (@target, $out);
 my $outfile = $ARGV[1];
 open(OUTFILE, ">>$outfile");
 
-undef $/;
-my $class = 'chaos';
+my $host = $target[0];
 
-while (1)
+unless ( $host =~ m/^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$/ ) {
+
+print OUTFILE 'Error: not valid domain name ', $host, "\n";
+
+}
+else
 {
-    open( READ, "nslookup -type=txt -cl=$class version.bind $target[0] |" );
-    $out = <READ>;
-    close(READ);
+    $host =~ s/^www\.//;
 
-    if ($class eq 'chaos' && $out =~ m/got version.bind\/TXT\/IN/si)
-    {
-        print OUTFILE "Possible misconfiguration - expects version.bind in IN-class record.\n\n";
-        $class = 'in';
-        next;
+    my $res = Net::DNS::Resolver->new();
+    my $cnt = 1;
+
+    undef $/;
+    
+    my $query = $res->query( $host, 'NS' );
+
+    if ( $query ) {
+
+        foreach my $rr ( grep { $_->type eq 'NS' } $query->answer ) {
+
+            print OUTFILE $cnt++, ". ", $rr->nsdname, ":\n";
+
+            my $class = 'chaos';
+            my $target = $rr->nsdname;
+
+            while (1)
+            {
+                
+                open( READ, "nslookup -type=txt -cl=$class version.bind $target |" );
+                $out = <READ>;
+                close(READ);
+
+                if ($class eq 'chaos' && $out =~ m/got version.bind\/TXT\/IN/si)
+                {
+                    print OUTFILE "Possible misconfiguration - expects version.bind in IN-class record.\n\n";
+                    $class = 'in';
+                    next;
+                }
+
+                last;
+            }
+
+            print OUTFILE $out;
+            print OUTFILE "\n";
+        }
     }
+    else { print OUTFILE "\n", $cnt++, ') Error: no NS for domain ', $host, ' (', $res->errorstring, ")\n"; }
 
-    last;
+    
 }
 
-print OUTFILE $out;
 close(OUTFILE);
 
 exit(0);
@@ -54,3 +88,4 @@ sub getinput {
   else { print q[Error: cannot open file ], $fi, "\n"; exit(0); }
 
 };
+
