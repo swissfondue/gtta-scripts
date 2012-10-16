8d5159e9c9d143dcae06c794394f0f5e6878e101
Anton Belousov <erbol.turburgaev@gmail.com>
2012-10-16 14:38:48 +0400

    Backed out changeset e7fddde98ba7 w3af_utils.py backed out.

--- pythonlib/w3af_utils.py
+++ pythonlib/w3af_utils.py
@@ -5,6 +5,7 @@ w3af communication utils
 
 import os
 import tempfile
+
 import call
 
 SCRIPT_TEMPLATE = '''target
@@ -12,14 +13,6 @@ set target {target}
 back
 http-settings
 set timeout {timeout}
-back
-plugins
-output !console textFile
-output config textFile
-set httpFileName /dev/null
-set verbose False
-set fileName {file_name}
-back
 back
 {script}
 start
@@ -29,25 +22,22 @@ exit
 W3AF_CMD = os.path.join(
     os.path.dirname(__file__), "w3af", "w3af_console")
 
-def call_w3af(target, commands, output_file, timeout=15):
+def call_w3af(target, commands, timeout=15):
     """
     Call w2af_console with a defined command sequence
     Result: (success: boolean, data: unicode)
     """
+
     script = SCRIPT_TEMPLATE.format(
-        target = target,
-        timeout = timeout,
-        script = '\n'.join(commands),
-        file_name = output_file
+        target=target,
+        timeout=timeout,
+        script='\n'.join(commands),
     )
 
     with tempfile.NamedTemporaryFile() as tmpf:
         tmpf.write(script)
         tmpf.file.flush()
-        
-        called_ok, output = call.call([ W3AF_CMD, "-s", tmpf.name ])
-
-        return called_ok, output, open(output_file).read()
+        return call.call([W3AF_CMD, "-s", tmpf.name])
 
 class W3AFScriptLauncher(object):
     """
@@ -62,61 +52,44 @@ class W3AFScriptLauncher(object):
     def main(self):
         target = self.host or self.ip
         protocol = self.proto or 'http'
-        output_file = tempfile.NamedTemporaryFile(delete=False)
-        output_file.close()
 
         self._check_stop()
-
-        called_ok, output, content = call_w3af(
-            target = '%s://%s' % (protocol, target),
-            timeout = self.SOCKET_TIMEOUT,
-            commands = self._get_commands(),
-            output_file = output_file.name
+        called_ok, output = call_w3af(
+            target='%s://%s' % (protocol, target),
+            timeout=self.SOCKET_TIMEOUT,
+            commands=self._get_commands()
         )
 
-        os.unlink(output_file.name)
         self._check_stop()
 
         if not called_ok:
             self._write_result('w3af_console launching error!')
 
         else:
-            data = iter(output.split('\n'))
-            self._check_stop()
+            data = iter(output.split('\r\n'))
 
+            self._check_stop()
             # collecting of the script errors
             param_errors = []
-
             for line in data:
                 if line.startswith('w3af>>> exit'):
                     param_errors.append('Unexpected end of script')
                     break
-
                 if line.startswith('w3af>>> start'):
                     break
-
                 if not line.startswith('w3af'):
                     param_errors.append(line)
-
             else:
                 param_errors.append('Unexpected end of script')
 
             if param_errors:
                 self._write_result('w3af configuration errors:\n')
                 self._write_result('\n'.join(param_errors))
-
                 return
 
             self._check_stop()
-
             # collecting the w3af output
-            if content:
-                for line in content.split('\n'):
-                    if line.find(' - information ] ') == -1:
-                        continue
-
-                    self._write_result(line[line.find('] ') + 2:])
-
-            else:
-                self._write_result('No output from w3af.')
-
+            for line in data:
+                if line.startswith('w3af>>> exit'):
+                    break
+                self._write_result(line)
