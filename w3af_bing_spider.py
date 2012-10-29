144cab1394af858a71dc41fe589114a0a4aeb4f1
Anton Belousov <erbol.turburgaev@gmail.com>
2012-10-30 03:40:46 +0400

    Output filter for w3af bing spider.

--- w3af_bing_spider.py
+++ w3af_bing_spider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from re import match
 from sys import path
 path.append('pythonlib')
 
@@ -25,7 +26,27 @@ class BingSpiderTask(gtta.Task, w3af_utils.W3AFScriptLauncher):
             "plugins",
             "discovery bing_spider",
             "grep getMails",
+            "grep config getMails",
+            "set onlyTargetDomain False",
+            "back",
             "back"
         ]
+
+    def _filter_result(self, result):
+        """
+        Filter w3af result
+        """
+        mails = []
+
+        for line in result:
+            mail = match(r'The mail account: "([^"]+)"', line)
+
+            if mail and not mail.groups()[0] in mails:
+                mails.append(mail.groups()[0])
+
+        if len(mails):
+            return 'Found %i e-mails:\n%s' % ( len(mails), '\n'.join(mails) )
+
+        return 'No e-mails found.'
 
 gtta.execute_task(BingSpiderTask)
