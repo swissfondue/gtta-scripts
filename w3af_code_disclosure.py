ba3bd71bdf50d73b16ea89eab2f853a9b7547642
Anton Belousov <erbol.turburgaev@gmail.com>
2012-10-30 04:05:48 +0400

    Output filter for w3af code disclosure.

--- w3af_code_disclosure.py
+++ w3af_code_disclosure.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from re import match
 from sys import path
 path.append('pythonlib')
 
@@ -27,5 +28,22 @@ class CodeDisclosureTask(gtta.Task, w3af_utils.W3AFScriptLauncher):
             "discovery webSpider",
             "back"
         ]
+
+    def _filter_result(self, result):
+        """
+        Filter w3af result
+        """
+        urls = []
+
+        for line in result:
+            url = match(r'The URL: "([^"]+)" has a (.*?) code disclosure vulnerability', line)
+
+            if url and not url.groups()[0] in urls:
+                urls.append('%s (%s)' % url.groups())
+
+        if len(urls):
+            return 'Found %i URLs with possible code disclosure:\n%s' % ( len(urls), '\n'.join(urls) )
+
+        return 'No URLs with code disclosure found.'
 
 gtta.execute_task(CodeDisclosureTask)
