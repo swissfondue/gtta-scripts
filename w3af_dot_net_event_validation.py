bb998f7fc71e5cb0de943a9b9085731fd3df3f07
Anton Belousov <erbol.turburgaev@gmail.com>
2012-10-30 14:41:53 +0400

    Output filter for w3af dot net event validation.

--- w3af_dot_net_event_validation.py
+++ w3af_dot_net_event_validation.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from re import match
 from sys import path
 path.append('pythonlib')
 
@@ -27,5 +28,35 @@ class DotNetEventValidationTask(gtta.Task, w3af_utils.W3AFScriptLauncher):
             "discovery webSpider",
             "back"
         ]
+
+    def _filter_result(self, result):
+        """
+        Filter w3af result
+        """
+        urls_validation = []
+        urls_encryption = []
+
+        for line in result:
+            url = match(r'The URL: "([^"]+)" has .NET Event Validation disabled.', line)
+
+            if url and not url.groups()[0] in urls_validation:
+                urls_validation.append(url.groups()[0])
+
+            url = match(r'The URL: "([^"]+)" has .NET ViewState encryption disabled.', line)
+
+            if url and not url.groups()[0] in urls_encryption:
+                urls_encryption.append(url.groups()[0])
+
+        msg = []
+
+        if len(urls_validation):
+            msg.append('Found %i URLs with disabled event validation (possible programming/configuration error):\n%s' % ( len(urls_validation), '\n'.join(urls_validation) ))
+
+        if len(urls_encryption):
+            msg.append('Found %i URLs with disabled ViewState encryption (exploitable programming/configuration error):\n%s' % ( len(urls_encryption), '\n'.join(urls_encryption) ))
+
+        return '\n\n'.join(msg)
+
+        return 'No URLs with incorrect event validation found.'
 
 gtta.execute_task(DotNetEventValidationTask)
