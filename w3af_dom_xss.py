03ad46951db1d9fd404c0ea5541a6fdd67c769a8
Anton Belousov <erbol.turburgaev@gmail.com>
2012-10-30 05:28:33 +0400

    Output filter for w3af DOM XSS.

--- w3af_dom_xss.py
+++ w3af_dom_xss.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from re import match
 from sys import path
 path.append('pythonlib')
 
@@ -27,5 +28,22 @@ class DomXSSTask(gtta.Task, w3af_utils.W3AFScriptLauncher):
             "discovery webSpider",
             "back"
         ]
+
+    def _filter_result(self, result):
+        """
+        Filter w3af result
+        """
+        vulns = []
+
+        for line in result:
+            vuln = match(r'The URL: "([^"]+)" has a DOM XSS \(Risky JavaScript Code\) bug using: "([^"]+)"', line)
+
+            if vuln and not ('%s (%s)' % vuln.groups()) in vulns:
+                vulns.append('%s (%s)' % vuln.groups())
+
+        if len(vulns):
+            return 'Found %i URLs with possible DOM XSS vulnerability:\n%s' % ( len(vulns), '\n'.join(vulns) )
+
+        return 'No URLs with DOM XSS vulnerability found.'
 
 gtta.execute_task(DomXSSTask)
