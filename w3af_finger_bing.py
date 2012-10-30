d7d3bad3504c2d18fbbb1ab0d4555ab2958e9d2c
Anton Belousov <erbol.turburgaev@gmail.com>
2012-10-31 01:43:41 +0400

    Output filter for w3af finger bing.

--- w3af_finger_bing.py
+++ w3af_finger_bing.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from re import match
 from sys import path
 path.append('pythonlib')
 
@@ -26,5 +27,22 @@ class FingerBingTask(gtta.Task, w3af_utils.W3AFScriptLauncher):
             "discovery fingerBing",
             "back"
         ]
+
+    def _filter_result(self, result):
+        """
+        Filter w3af result
+        """
+        mails = []
+
+        for line in result:
+            mail = match(r'The mail account: "([^"]+)"', line)
+
+            if mail and not mail.groups()[0] in mails:
+                mails.append(mail.groups()[0])
+
+        if len(mails):
+            return 'Found %i e-mails:\n%s' % ( len(mails), '\n'.join(mails) )
+
+        return 'No e-mails found.'
 
 gtta.execute_task(FingerBingTask)
