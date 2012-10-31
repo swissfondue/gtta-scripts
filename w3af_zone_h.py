25e2269db86f08961f937e527bf0b2212fc2b44e
Anton Belousov <erbol.turburgaev@gmail.com>
2012-10-31 20:17:38 +0400

    Output filter for w3af zone h.

--- w3af_zone_h.py
+++ w3af_zone_h.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from re import match
 from sys import path
 path.append('pythonlib')
 
@@ -26,5 +27,33 @@ class ZoneHTask(gtta.Task, w3af_utils.W3AFScriptLauncher):
             "discovery zone_h",
             "back"
         ]
+
+    def _filter_result(self, result):
+        """
+        Filter w3af result
+        """
+        output = []
+
+        for line in result:
+            out_line = match(r'The target site was defaced more than one time in the past. For more information please visit the following URL: "([^"]+)"', line)
+
+            if out_line:
+                out_line = 'The target was defaced more than one time. Please see %s for more info.' % out_line.groups()[0]
+
+                if out_line not in output:
+                    output.append(out_line)
+
+            out_line = match(r'The target site was defaced in the past. For more information please visit the following URL: "([^"]+)"', line)
+
+            if out_line:
+                out_line = 'The target was defaced in the past. Please see %s for more info.' % out_line.groups()[0]
+
+                if out_line not in output:
+                    output.append(out_line)
+
+        if len(output):
+            return '\n'.join(output)
+
+        return 'The target is not listed in the Zone-H database.'
 
 gtta.execute_task(ZoneHTask)
