f0cb14fec3c60bcfbb3792da3154e94ff85fe4d2
Anton Belousov <erbol.turburgaev@gmail.com>
2012-10-31 15:32:41 +0400

    Output filter for w3af phishtank.

--- w3af_phishtank.py
+++ w3af_phishtank.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from re import match
 from sys import path
 path.append('pythonlib')
 
@@ -26,5 +27,23 @@ class PhishtankTask(gtta.Task, w3af_utils.W3AFScriptLauncher):
             "discovery phishtank",
             "back"
         ]
+
+    def _filter_result(self, result):
+        """
+        Filter w3af result
+        """
+        output = None
+
+        for line in result:
+            output_str = match(r'The URL: "([^"]+)" seems to be involved in a phishing scam. Please see "([^"]+)" for more info', line)
+
+            if output_str:
+                output = 'The target is listed in the PhishTank database: %s\nPlease see %s for more info.' % output_str.groups()
+                break
+
+        if output:
+            return output
+
+        return 'The target is not listed in the PhishTank database.'
 
 gtta.execute_task(PhishtankTask)
