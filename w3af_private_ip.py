0adcdd20028fe7ed660cc44d3dcdc5f73b827f68
Anton Belousov <erbol.turburgaev@gmail.com>
2012-10-31 17:18:39 +0400

    Output filter for w3af private ip.

--- w3af_private_ip.py
+++ w3af_private_ip.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from re import match
 from sys import path
 path.append('pythonlib')
 
@@ -27,5 +28,42 @@ class PrivateIPTask(gtta.Task, w3af_utils.W3AFScriptLauncher):
             "discovery webSpider",
             "back"
         ]
+
+    def _filter_result(self, result):
+        """
+        Filter w3af result
+        """
+        header_ips = []
+        html_ips = []
+
+        for line in result:
+            ip = match(r'The URL: "([^"]+)" returned an HTTP header with an IP address: "([^"]+)"\.', line)
+
+            if ip:
+                ip = '%s (%s)' % ip.groups()
+
+                if ip not in header_ips:
+                    header_ips.append(ip)
+
+            ip = match(r'The URL: "([^"]+)" returned an HTML document with an IP address: "([^"]+)"\.', line)
+
+            if ip:
+                ip = '%s (%s)' % ip.groups()
+
+                if ip not in html_ips:
+                    html_ips.append(ip)
+
+        msg = []
+
+        if len(header_ips):
+            msg.append('Found %i URLs with IP address in HTTP header:\n%s' % ( len(header_ips), '\n'.join(header_ips) ))
+
+        if len(html_ips):
+            msg.append('Found %i URLs with IP address in HTML document:\n%s' % ( len(html_ips), '\n'.join(html_ips) ))
+
+        if msg:
+            return '\n\n'.join(msg)
+
+        return 'No IP addresses found.'
 
 gtta.execute_task(PrivateIPTask)
