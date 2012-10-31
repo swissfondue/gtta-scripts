03a078f1288f86aad09245f91a3b14577f8c2080
Anton Belousov <erbol.turburgaev@gmail.com>
2012-10-31 04:43:22 +0400

    Output filter for w3af ghdb.

--- w3af_ghdb.py
+++ w3af_ghdb.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from re import match
 from sys import path
 path.append('pythonlib')
 
@@ -26,5 +27,25 @@ class GHDBTask(gtta.Task, w3af_utils.W3AFScriptLauncher):
             "discovery ghdb",
             "back"
         ]
+
+    def _filter_result(self, result):
+        """
+        Filter w3af result
+        """
+        vulns = []
+
+        for line in result:
+            vuln = match(r'ghdb plugin found a vulnerability at URL: ([^ ]+) . Vulnerability description: (.*)', line)
+
+            if vuln:
+                vuln = '%s (%s)' % vuln.groups()
+
+                if vuln not in vulns:
+                    vulns.append(vuln)
+
+        if len(vulns):
+            return 'Found %i GHDB vulnerabilities:\n%s' % ( len(vulns), '\n'.join(vulns) )
+
+        return 'No GHDB vulnerabilities found.'
 
 gtta.execute_task(GHDBTask)
