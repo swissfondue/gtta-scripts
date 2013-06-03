4b0e2c37cd05a414b44ef92e2260ade1c1383ee5
Anton Belousov <erbol.turburgaev@gmail.com>
2013-06-03 07:44:21 +0400

    GTTA-21

--- params_crawler.py
+++ params_crawler.py
@@ -10,29 +10,36 @@ from HTMLParser import HTMLParser
 from crawler import LinkCrawler
 from gtta import Task, execute_task
 
-
 class FormsParser(HTMLParser):
-
+    """
+    Form parser
+    """
     form_attributes = dict()
 
     def __init__(self):
+        """
+        Constructor
+        """
         HTMLParser.__init__(self)
 
     def handle_starttag(self, tag, attrs):
+        """
+        Handle start tag
+        """
         at = dict(attrs)
+
         if tag == 'form':
             self.form_attributes = dict()
             self.form_attributes['action'] = at['action']
             self.form_attributes['inputs'] = []
-        if tag == 'input':
-            self.form_attributes['inputs'].append(at)
 
+        if tag in ('input', 'select'):
+            self.form_attributes['inputs'].append(at)
 
 class Params_Craw(Task):
     """
     Complete parameters from forms and urls, using crawler.py
     """
-
     urls_set = set()
     forms_urls_set = set()
     form_parser = FormsParser()
@@ -48,21 +55,23 @@ class Params_Craw(Task):
 
     def collect_params(self, url):
         """ Collecting params from link """
-
         if url not in self.urls_set:
             splitted_url = urlsplit(url)
             query_params = parse_qs(splitted_url[3])
+
             if query_params:
-                self._write_result(splitted_url[0] + '://' + splitted_url[1] + splitted_url[2])
-                #self._write_result(url)  # Need to know format: up or this
+                self._write_result(url)
+
                 for key, value in query_params.items():
                     self._write_result('\t' + key + '=' + value[0])
                     self._check_stop()
+
+                self._write_result("")
+
             self.collect_unique_urls(url)
 
     def collect_form_params(self, page):
         """ If we have on page form, then outputting that form url and params from there"""
-
         if page['url'] not in self.forms_urls_set:
             self.forms_urls_set.add(page['url'])
 
@@ -71,17 +80,21 @@ class Params_Craw(Task):
 
                 self.form_parser.feed(item.group(0))  # Parsing form action and inputs params with Form_Parser
                 form_attributes = self.form_parser.form_attributes
-                self._write_result('form action: ' + form_attributes['action'])
+                self._write_result('Form: ' + form_attributes['action'])
 
                 for input_params in form_attributes['inputs']:  # Iterating all inputs attributes
                     self._check_stop()
-                    input_params_formatted = ''
 
-                    for key, value in input_params.items():
-                        self._check_stop()
-                        input_params_formatted += (key + '=' + value + ',')
+                    if 'name' in input_params:
+                        name = input_params['name']
+                        value = ''
 
-                    self._write_result('\t' + input_params_formatted)
+                        if 'value' in input_params:
+                            value = input_params['value']
+
+                        self._write_result("\t%s=%s" % (name, value))
+
+                self._write_result("")
 
     def main(self):
         """
@@ -97,7 +110,6 @@ class Params_Craw(Task):
 
         if self.host:
             target = self.proto + '://' + self.host + '/'
-
         else:
             target = self.proto + '://' + self.ip + '/'
 
@@ -106,7 +118,6 @@ class Params_Craw(Task):
         self._check_stop()
 
         if not self.produced_output:
-            self._write_result('No link to any documents found')
-
+            self._write_result('No variables found.')
 
 execute_task(Params_Craw)
