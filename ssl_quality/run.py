589a4c76265a4cc60a82fd8c5e752196eaa74abd
Anton Belousov <anton@stellarbit.com>
2014-02-13 02:23:00 +0400

    GTTA-145

--- ssl_quality/run.py
+++ ssl_quality/run.py
@@ -1,20 +1,37 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
-from subprocess import call
+from subprocess import Popen
+from threading import Timer
 from core import Task, execute_task
+
 
 class SSLQualityTask(Task):
     """
     SSL quality checker
     """
-    _PROTOCOLS = ('ssl2', 'ssl3', 'tls1', 'tls1_1', 'tls1_2')
-    _PROTOCOL_NAMES = {
-        'ssl2': 'SSL 2.0',
-        'ssl3': 'SSL 3.0',
-        'tls1': 'TLS 1.0',
-        'tls1_1': 'TLS 1.1',
-        'tls1_2': 'TLS 1.2'
+    TIMEOUT = 120
+    TEST_TIMEOUT = 60
+    PROTOCOL_TIMEOUT = 5
+
+    PROTOCOLS = ("ssl2", "ssl3", "tls1", "tls1_1", "tls1_2")
+    PROTOCOL_NAMES = {
+        "ssl2": "SSL 2.0",
+        "ssl3": "SSL 3.0",
+        "tls1": "TLS 1.0",
+        "tls1_1": "TLS 1.1",
+        "tls1_2": "TLS 1.2"
     }
+
+    @staticmethod
+    def timeout(process):
+        """
+        Process timeout handler
+        """
+        if process.poll() is None:
+            try:
+                process.kill()
+            except:
+                pass
 
     def main(self, *args):
         """
@@ -30,25 +47,30 @@ class SSLQualityTask(Task):
         if not port:
             port = 443
 
-        for protocol in self._PROTOCOLS:
-            return_code = call(
+        for protocol in self.PROTOCOLS:
+            process = Popen(
                 'echo "GET /" | openssl s_client -%s -connect %s:%i' % (protocol, target, port),
                 shell=True
             )
 
+            timer = Timer(self.PROTOCOL_TIMEOUT, self.timeout, [process])
+            timer.start()
+            process.wait()
+            timer.cancel()
+
             result = "No"
 
-            if return_code == 0:
+            if process.returncode == 0:
                 result = "Yes"
 
-            self._write_result("%s: %s" % (self._PROTOCOL_NAMES[protocol], result))
+            self._write_result("%s: %s" % (self.PROTOCOL_NAMES[protocol], result))
 
     def test(self):
         """
         Test function
         """
         self.port = 443
-        self.host = "www.google.com"
+        self.host = "google.com"
         self.main()
 
 execute_task(SSLQualityTask)
