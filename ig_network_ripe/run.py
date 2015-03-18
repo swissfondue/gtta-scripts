c3abdfc83c1c4ddb88749a001fab83946613f8a4
user <user@debian.248>
2015-03-18 14:45:49 +0300

    GTTA-393

--- ig_network_ripe/run.py
+++ ig_network_ripe/run.py
@@ -3,39 +3,50 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 from core import Task, execute_task
-from core.error import NoHostName
+from core.error import InvalidTarget
 
 
 class IG_Network_Ripe(Task):
     """
     Get ripe inetnums
     """
+    target = ''
+
+    def _get_soup(self, session, data):
+        """
+        Getting soup object from post request
+        """
+        return BeautifulSoup(
+            session.post(
+                'https://apps.db.ripe.net/search/full-text.html',
+                headers={'User-Agent': 'Mozilla/5.0'},
+                data=data
+            ).content)
+
+    def _get_state(self, raw):
+        """
+        Getting javax.faces.ViewState from html page
+        """
+        return raw.find('input', attrs={'name': 'javax.faces.ViewState', 'type': 'hidden'}).attrMap['value']
+
+    def _collect_data_from_page(self, raw, res):
+        """
+        Collecting data from page
+        """
+        form = raw.find('div', attrs={'id': 'form'})
+        for tag in form.find('fieldset').findAll('div')[1].findAll('a'):
+            res.append(tag.text.replace('inetnum: ', ''))
 
     def main(self, *args):
         """
         Main function
         """
-        if not self.term:
-            raise NoHostName('No term specified.')
+        if not self.target:
+            raise InvalidTarget('No target specified.')
 
         self._check_stop()
 
-        def _get_state(raw):
-            """
-            Getting javax.faces.ViewState from html page
-            """
-            return raw.find('input', attrs={'name': 'javax.faces.ViewState', 'type': 'hidden'}).attrMap['value']
-
-        def _collect_data_from_page(raw):
-            """
-            Collecting data from page
-            """
-            form = raw.find('div', attrs={'id': 'form'})
-            for tag in form.find('fieldset').findAll('div')[1].findAll('a'):
-                results.append(tag.text.replace('inetnum: ', ''))
-
         results = []
-        headers = {'User-Agent': 'Mozilla/5.0'}
         advanced_form_data = {
             'home_search': 'home_search',
             'home_search:searchform_q:': '',
@@ -44,7 +55,7 @@ class IG_Network_Ripe(Task):
         action_form_data = {
             'home_search': 'home_search',
             'home_search:doSearch': 'Search',
-            'home_search:searchform_q': 'clariant',
+            'home_search:searchform_q': self.target,
             'home_search:searchTypeSubview:typeSelectBox': '1',
             'home_search:advancedSearch:selectObjectType': 'inetnum',
         }
@@ -55,48 +66,31 @@ class IG_Network_Ripe(Task):
         session = requests.Session()
 
         # first visit on the site (get javax.faces.ViewState)
-        soup = BeautifulSoup(
-            session.post(
-                'https://apps.db.ripe.net/search/full-text.html',
-                headers=headers,
-                data={}
-            ).content)
-        advanced_form_data.update({'javax.faces.ViewState': _get_state(soup)})
+        soup = self._get_soup(session, {})
+        advanced_form_data.update({'javax.faces.ViewState': self._get_state(soup)})
 
         # select Advanced Search
-        soup = BeautifulSoup(
-            session.post(
-                'https://apps.db.ripe.net/search/full-text.html',
-                headers=headers,
-                data=advanced_form_data
-            ).content)
-        action_form_data.update({'javax.faces.ViewState': _get_state(soup)})
+        soup = self._get_soup(session, advanced_form_data)
+        action_form_data.update({'javax.faces.ViewState': self._get_state(soup)})
 
         # get first page of results
-        soup = BeautifulSoup(
-            session.post(
-                'https://apps.db.ripe.net/search/full-text.html',
-                headers=headers,
-                data=action_form_data
-            ).content)
-        page_form.update({'javax.faces.ViewState': _get_state(soup)})
+        soup = self._get_soup(session, action_form_data)
+        page_form.update({'javax.faces.ViewState': self._get_state(soup)})
+
+        # collect data from first page
+        self._collect_data_from_page(soup, results)
 
         # go to last page
-        soup = BeautifulSoup(
-            session.post(
-                'https://apps.db.ripe.net/search/full-text.html',
-                headers=headers,
-                data=page_form
-            ).content)
-        page_form['javax.faces.ViewState'] = _get_state(soup)
+        soup = self._get_soup(session, page_form)
+        page_form['javax.faces.ViewState'] = self._get_state(soup)
 
         # collect data from current page
-        _collect_data_from_page(soup)
+        self._collect_data_from_page(soup, results)
 
         # collect data from prev pages
         current = int(soup.find('span', attrs={'id': 'current'}).text)
         del page_form['resultsView:paginationView:paginationForm:main:last:last']
-        while current > 1:
+        while current > 2:
             current -= 1
             # find link to previous page in paginator
             paginator = soup.find('form', attrs={'id': 'resultsView:paginationView:paginationForm'})
@@ -107,17 +101,12 @@ class IG_Network_Ripe(Task):
                     page_form.update({page_link_name: page_link_val})
                     break
             # get previous page of results
-            soup = BeautifulSoup(
-                session.post(
-                    'https://apps.db.ripe.net/search/full-text.html',
-                    headers=headers,
-                    data=page_form
-                ).content)
+            soup = self._get_soup(session, page_form)
             if page_link_name in page_form.keys():
                 del page_form[page_link_name]
-            page_form['javax.faces.ViewState'] = _get_state(soup)
+            page_form['javax.faces.ViewState'] = self._get_state(soup)
             # collect data from previous page
-            _collect_data_from_page(soup)
+            self._collect_data_from_page(soup, results)
 
         self._check_stop()
 
@@ -133,7 +122,7 @@ class IG_Network_Ripe(Task):
         """
         Test function
         """
-        self.term = "clariant"
+        self.target = "clariant"
         self.main()
 
 execute_task(IG_Network_Ripe)
