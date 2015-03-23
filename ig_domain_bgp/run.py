9d703fe094e23776b6f2eeff4319fe049f8a60da
Anton Belousov <anton@belousov.co>
2015-03-23 11:38:07 +0300

    GTTA-406 refactoring

--- ig_domain_bgp/run.py
+++ ig_domain_bgp/run.py
@@ -1,78 +1,52 @@
 # -*- coding: utf-8 -*-
 import requests
-import socket
+from socket import gethostbyname
 from BeautifulSoup import BeautifulSoup
 from core import Task, execute_task
-from core.error import InvalidTarget
-
-__author__ = 'user'
 
 
 class IG_Domain_BGP(Task):
     """
-    Searching records in BGP
+    Search records in BGP
     """
-    target = ''
-
-    def _is_ip_v4(self, address):
-        """
-        Check adress format is IPv4
-        """
-        try:
-            socket.inet_aton(address)
-            result = True
-        except socket.error:
-            result = False
-        return result
-
-    def _search_by_ip(self, ip, res):
-        """
-        Searching by ip
-        """
-        soup = BeautifulSoup(
-            requests.get(
-                'http://bgp.he.net/ip/%s' % ip,
-                headers={'User-Agent': 'Mozilla/5.0'}
-            ).content)
-        div = soup.find('div', attrs={'id': 'dns'})
-        for tag in div.findAll('a'):
-            res.append(tag.text)
-            self._write_result(tag.text)
 
     def main(self, *args):
         """
         Main function
         """
-        if not self.target:
-            raise InvalidTarget('No target specified.')
-
-        self._check_stop()
-        self._write_result('target = "%s"' % self.target)
         results = []
 
-        if self._is_ip_v4(self.target):
-            self._search_by_ip(self.target, results)
-        else:
+        if not self.ip:
             try:
-                ip = socket.gethostbyname(self.target)
-                self._search_by_ip(ip, results)
+                self.ip = gethostbyname(self.host)
             except:
-                pass
+                return
 
-        self._check_stop()
+        soup = BeautifulSoup(
+            requests.get(
+                'http://bgp.he.net/ip/%s' % self.ip,
+                headers={'User-Agent': 'Mozilla/5.0'}
+            ).content
+        )
 
-        if len(results) == 0:
-            self._write_result('No BGP records.')
+        div = soup.find('div', attrs={'id': 'dns'})
+
+        for tag in div.findAll('a'):
+            result = tag.text
+
+            if result not in results:
+                results.append(result)
+                self._write_result(result)
 
     def test(self):
         """
         Test function
         """
-        self.target = "clariant"
+        self.host = "clariant"
         self.main()
-        self.target = "83.150.1.145"
+        self.ip = "83.150.1.145"
         self.main()
-        self.target = "clariant.com"
+        self.host = "clariant.com"
         self.main()
 
 execute_task(IG_Domain_BGP)
