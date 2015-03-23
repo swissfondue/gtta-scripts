47998a003508657e808bb015d6e7561a22d59cc1
Anton Belousov <anton@belousov.co>
2015-03-23 10:30:29 +0300

    GTTA-395

--- ig_network_bgp/run.py
+++ ig_network_bgp/run.py
@@ -3,7 +3,6 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 from core import Task, execute_task
-from core.error import InvalidTarget
 
 
 class IG_Network_BGP(Task):
@@ -12,70 +11,64 @@ class IG_Network_BGP(Task):
     """
     target = ''
 
-    def _valid_res_add(self, text, results):
+    def _add_valid_result(self, text):
         """
-        Filtering by '.' (not AS and not IPv6)
+        Filter by '.' (not AS and not IPv6)
         """
         if '.' in text:
             self._write_result(text)
-            results.append(text)
 
-    def _search_by_keyword(self, raw, res):
+    def _search_by_keyword(self, raw):
         """
-        Searching by keyword
+        Search by keyword
         """
         div = raw.find('div', attrs={'id': 'search'})
-        for tag in div.findAll('a'):
-            self._valid_res_add(tag.text, res)
 
-    def _search_by_ip(self, raw, res):
+        for tag in div.findAll('a'):
+            self._add_valid_result(tag.text)
+
+    def _search_by_ip(self, raw):
         """
-        Searching by ip
+        Search by ip
         """
         div = raw.find('div', attrs={'id': 'ipinfo'})
-        for tag in div.findAll('td', attrs={'class': 'nowrap'}):
-            self._valid_res_add(tag.find('a').text, res)
 
-    def _search_by_domain(self, raw, res):
+        for tag in div.findAll('td', attrs={'class': 'nowrap'}):
+            self._add_valid_result(tag.find('a').text)
+
+    def _search_by_domain(self, raw):
         """
-        Searching by domain
+        Search by domain
         """
         div = raw.find('div', attrs={'id': 'dns'})
+
         for dnshead in div.findAll('div', attrs={'class': 'dnshead'}):
             if dnshead.text == 'A Records':
                 dnsdata = dnshead.nextSibling.nextSibling
-                self._valid_res_add(dnsdata.find('a').text, res)
+                self._add_valid_result(dnsdata.find('a').text)
 
     def main(self, *args):
         """
         Main function
         """
-        if not self.target:
-            raise InvalidTarget('No target specified.')
-
-        self._check_stop()
-        self._write_result('target = "%s"' % self.target)
-        results = []
-
         soup = BeautifulSoup(
             requests.get(
                 'http://bgp.he.net/search',
                 params={'search[search]': self.target, 'commit': 'Search'},
                 headers={'User-Agent': 'Mozilla/5.0'}
-            ).content)
+            ).content
+        )
+
         first_tabmenuli_id = soup.find('li', attrs={'class': 'tabmenuli'}).attrMap['id']
 
         if first_tabmenuli_id == 'tab_search':
-            self._search_by_keyword(soup, results)
-        if first_tabmenuli_id == 'tab_ipinfo':
-            self._search_by_ip(soup, results)
-        if first_tabmenuli_id == 'tab_dns':
-            self._search_by_domain(soup, results)
+            self._search_by_keyword(soup)
+        elif first_tabmenuli_id == 'tab_ipinfo':
+            self._search_by_ip(soup)
+        elif first_tabmenuli_id == 'tab_dns':
+            self._search_by_domain(soup)
 
         self._check_stop()
-
-        if len(results) == 0:
-            self._write_result('No BGP records.')
 
     def test(self):
         """
