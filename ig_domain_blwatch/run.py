422d6672461e307e48c7bf55075d25a2512d941c
user <user@debian.248>
2015-03-25 20:14:32 +0300

    GTTA-407

---
+++ ig_domain_blwatch/run.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import requests
+from BeautifulSoup import BeautifulSoup
+from core import Task, execute_task
+
+
+class IG_Domain_BLWatch(Task):
+    """
+    Search records in BackLinkWatcher
+    """
+
+    def main(self, *args):
+        """
+        Main function
+        """
+        results = []
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36'}
+        index_params = {
+            'backlinkurl': self.host,
+            'submit': 'Check Backlinks'
+        }
+        live_params = {
+            'feed': '3660785',
+            '_': ''
+        }
+        ses = requests.Session()
+        soup = BeautifulSoup(
+            ses.get(
+                'http://www.backlinkwatch.com',
+                headers=headers,
+            ).content)
+
+        salt = soup.find('input', attrs={'type': 'hidden', 'name': 'salt'}).attrMap['value']
+
+        index_params.update({'salt': salt})
+
+        req = ses.post(
+                'http://www.backlinkwatch.com/index.php#',
+                headers=headers,
+                params=index_params,
+                prefetch=True,
+                timeout=10000
+            )
+        soup = BeautifulSoup(req.content)
+        soup = BeautifulSoup(req.content)
+
+
+    def test(self):
+        """
+        Test function
+        """
+        self.host = "clariant.com"
+        self.main()
+
+# execute_task(IG_Domain_BLWatch)
+IG_Domain_BLWatch().test()
