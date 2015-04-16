e303a37efe5aca4adb76c4ac1a6eb0639fae44ec
user <user@debian.248>
2015-04-16 15:54:42 +0300

    GTTA-411

---
+++ ig_subdomain_fierce/run.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from core import Task, execute_task
+import subprocess
+
+
+class IG_Subdomain_Fierce(Task):
+    """
+    Search records with fierce.pl
+    """
+
+    def main(self, *args):
+        """
+        Main function
+        """
+        if self.ip:
+            return
+
+        proc = subprocess.Popen('perl fierce.pl -dns %s' % self.host, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        res = proc.communicate()
+        strings = filter(lambda x: len(x), res[0].split('\n'))
+        res_strings = filter(lambda x: x[0].isdigit(), strings)
+        results = [string.split('\t')[1] for string in res_strings]
+        map(lambda x: self._write_result(x), results)
+
+    def test(self):
+        """
+        Test function
+        """
+        self.host = "clariant.com"
+        self.main()
+
+execute_task(IG_Subdomain_Fierce)
