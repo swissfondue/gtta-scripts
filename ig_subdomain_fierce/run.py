3b4f511d734cd6f05f39b363e4536fdae821e19a
user <user@debian.248>
2015-04-21 17:31:34 +0300

    GTTA-411

--- ig_subdomain_fierce/run.py
+++ ig_subdomain_fierce/run.py
@@ -16,11 +16,15 @@ class IG_Subdomain_Fierce(Task):
         if self.ip:
             return
 
+        results = set()
+
         proc = subprocess.Popen('perl fierce.pl -dns %s' % self.host, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         res = proc.communicate()
+
         strings = filter(lambda x: len(x), res[0].split('\n'))
         res_strings = filter(lambda x: x[0].isdigit(), strings)
-        results = [string.split('\t')[1] for string in res_strings]
+        map(lambda x: results.add(x.split('\t')[1]), res_strings)
+
         map(lambda x: self._write_result(x), results)
 
     def test(self):
