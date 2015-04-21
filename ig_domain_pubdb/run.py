93725887256c2e72d559e789487d5322a77441f1
user <user@debian.248>
2015-04-21 19:40:02 +0300

    GTTA-404

--- ig_domain_pubdb/run.py
+++ ig_domain_pubdb/run.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
+from time import sleep
 import requests
 from BeautifulSoup import BeautifulSoup
 from core import Task, execute_task
@@ -10,7 +11,11 @@ class IG_Domain_PubDB(Task):
     Search records in PubDB
     """
     URL = 'http://pub-db.com'
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36'}
     pubs = set()
     uas = set()
 
@@ -31,13 +36,13 @@ class IG_Domain_PubDB(Task):
         """
         Exctract pubs from content
         """
-        return re.compile('pub-\d{16}').findall(content)
+        return re.findall(r'pub-\d{16}', content)
 
     def _exctract_uas(self, content):
         """
         Exctract uas from content
         """
-        return re.compile('UA-\d{8}').findall(content)
+        return re.findall(r'UA-\d{8}', content)
 
     def main(self, *args):
         """
@@ -80,28 +85,42 @@ class IG_Domain_PubDB(Task):
 
         # search by collected links
         for href in hrefs:
-            soup = self._get_soup_by_path('%s' % href)
+            sleep(1)
+            try:
+                soup = self._get_soup_by_path('%s' % href)
+            except:
+                continue
             if soup.find('title').text == '404 Not Found':
                 continue
             div = soup.find('div', attrs={'id': 'stat'})
+            if not div:
+                continue
             self.pubs.update(self._exctract_pubs(div.text))
             self.uas.update(self._exctract_uas(div.text))
 
         # get domains by pubs
         for pub in self.pubs:
-            soup = self._get_soup_by_path('/adsense/%s.html' % pub)
+            sleep(1)
+            try:
+                soup = self._get_soup_by_path('/adsense/%s.html' % pub)
+            except:
+                continue
             for li in soup.findAll('li'):
                 domain = li.find('a').text
                 if not domain == self.target:
-                    results.update([domain])
+                    results.add(domain)
 
         # get domains by uas
         for ua in self.uas:
-            soup = self._get_soup_by_path('/google-analytics/%s.html' % ua)
+            sleep(1)
+            try:
+                soup = self._get_soup_by_path('/google-analytics/%s.html' % ua)
+            except:
+                continue
             for li in soup.findAll('li'):
                 domain = li.find('a').text
                 if not domain == self.target:
-                    results.update([domain])
+                    results.add(domain)
 
         # output results
         map(lambda x: self._write_result(x), results)
