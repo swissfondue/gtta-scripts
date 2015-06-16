94f74eed647203c78bdb1e0d30f41a44cd162c17
Anton Belousov <anton@belousov.co>
2015-06-16 10:33:04 +0300

    Backout changeset d1c2ddc6686df1efa860aaf3f3e95cb8ba14da3a

--- subdomain_bruteforce/run.pl
+++ subdomain_bruteforce/run.pl
@@ -13,20 +13,16 @@ class Subdomain_Bruteforce extends Task {
     use core::resulttable;
     use threads::shared;
 
-    use constant MULTITHREADED => 1;
-
-    my @threads;
-    my $last_t : shared = 0;
-
-    my $known_names = {};
-    my $domains = [];
-    my $nameservers = [];
-    my $wildcard_dns;
+    has "nameservers" => (is => "rw");
+    has "wildcard_dns" => (isa => "Str", is => "rw");
+    has "domains" => (is => "rw");
+    has "known_names" => (is => "rw");
 
     # Search host IP
     method search_host($search_item, $target) {
         my $res = Net::DNS::Resolver->new;
         $res->tcp_timeout(30);
+        #$res->nameservers(@{$self->nameservers});
         my $packet = $res->search($search_item);
 
         return unless ($packet);
@@ -38,116 +34,99 @@ class Subdomain_Bruteforce extends Task {
             chop $name[0];
             my $this_ip;
 
-            if ($name[4] eq $wildcard_dns) {
+            if ($name[4] eq $self->wildcard_dns) {
                 next;
             }
 
             $this_ip = $name[4];
-            next if ($known_names->{"$this_ip,$search_item"});
+            next if ($self->known_names->{"$this_ip,$search_item"});
 
-            $known_names->{"$this_ip,$search_item"} = 1;
-            push($domains, shared_clone([$search_item, $this_ip]));
+            $self->known_names->{"$this_ip,$search_item"} = 1;
+            push(@{$self->domains}, shared_clone([$search_item, $this_ip]));
         }
     }
     
     # Process
-    method _process(Int $big_file) {
-        while (1) {
-            my $seq = $last_t++;
+    method _process(Str $target, Int $big_file) {
+        if ($target !~ /[a-z\d.-]\.[a-z]*/i) {
+            die("Invalid domain name.");
+        }
 
-            if ($seq >= @{$self->targets}) {
-                return;
+        $self->domains(shared_clone([]));
+        $self->known_names(shared_clone({}));
+        $self->nameservers(shared_clone([]));
+
+        my $res = Net::DNS::Resolver->new;
+        $res->tcp_timeout(30);
+        my $query = $res->query($target, 'NS');
+
+        if ($query) {
+            foreach my $rr (grep {$_->type eq 'NS'} $query->answer) {
+                my $dnssrv = $rr->nsdname;
+                push (@{$self->nameservers}, $rr->nsdname);
+            }
+        }
+
+        $res->nameservers(@{$self->nameservers});
+
+        my @common_cnames = ();
+        my $hosts = q[files/hosts_small.txt];
+        $hosts = q[files/hosts_big.txt] if ($big_file);
+
+        if (-e $hosts) {
+            open (WORDLIST, '<', $hosts) or die("Can't open $hosts");
+
+            for (<WORDLIST>) {
+                chomp;
+                push @common_cnames, $_;
             }
 
-            my $target = $self->targets->[$seq];
+            close WORDLIST;
+        } else {
+            die("Can't open $hosts");
+        }
 
-            if ($target !~ /[a-z\d.-]\.[a-z]*/i) {
-                die("Invalid domain name.");
-            }
+        srand;
 
-            $domains = shared_clone([]);
-            $known_names = shared_clone({});
-            $nameservers = shared_clone([]);
+        $self->wildcard_dns(1e11 - int(rand(1e10)));
 
-            my $res = Net::DNS::Resolver->new;
-            $res->tcp_timeout(30);
-            my $query = $res->query($target, 'NS');
+        if (my $h = gethost($self->wildcard_dns . ".$target")) {
+            my $wildcard_addr = inet_ntoa($h->addr);
+            $self->wildcard_dns($wildcard_addr);
+            push(@{$self->domains}, shared_clone(["Wildcard", $wildcard_addr]));
+        } else {
+            $self->wildcard_dns("");
+        }
 
-            if ($query) {
-                foreach my $rr (grep {$_->type eq 'NS'} $query->answer) {
-                    my $dnssrv = $rr->nsdname;
-                    push ($nameservers, $rr->nsdname);
-                }
-            }
+        foreach my $host (@common_cnames) {
+            $self->search_host("$host.$target", $target);
+        }
 
-            $res->nameservers($nameservers);
+        unless (@{$self->domains}) {
+            $self->_write_result("No subdomains found.");
+        } else {
+            my $table = ResultTable->new(columns => [
+                {name => "#", width => "0.2"},
+                {name => "Domain", width => "0.5"},
+                {name => "IP", width => "0.3"}
+            ]);
 
-            my @common_cnames = ();
-            my $hosts = q[files/hosts_small.txt];
-            $hosts = q[files/hosts_big.txt] if ($big_file);
-
-            if (-e $hosts) {
-                open (WORDLIST, '<', $hosts) or die("Can't open $hosts");
-
-                for (<WORDLIST>) {
-                    chomp;
-                    push @common_cnames, $_;
-                }
-
-                close WORDLIST;
-            } else {
-                die("Can't open $hosts");
-            }
-
-            srand;
-
-            $wildcard_dns = 1e11 - int(rand(1e10));
-
-            if (my $h = gethost($wildcard_dns . ".$target")) {
-                my $wildcard_addr = inet_ntoa($h->addr);
-                $wildcard_dns = $wildcard_addr;
-                push($domains, shared_clone(["Wildcard", $wildcard_addr]));
-            } else {
-                $wildcard_dns = "";
-            }
-
-            foreach my $host (@common_cnames) {
-                $self->search_host("$host.$target", $target);
-            }
-
-            unless ($domains) {
-                $self->_write_result("No subdomains found.");
-            } else {
-                my $table = ResultTable->new(columns => [
-                    {name => "#", width => "0.2"},
-                    {name => "Domain", width => "0.5"},
-                    {name => "IP", width => "0.3"}
+            for (my $i = 0; $i < scalar(@{$self->domains}); $i++) {
+                $table->add_row([
+                    $i + 1,
+                    $self->domains->[$i][0],
+                    $self->domains->[$i][1]
                 ]);
-
-                for (my $i = 0; $i < scalar(@{$self->domains}); $i++) {
-                    $table->add_row([
-                        $i + 1,
-                        $domains->[$i][0],
-                        $domains->[$i][1]
-                    ]);
-                }
-
-                $self->_write_result($table->render());
             }
+
+            $self->_write_result($table->render());
         }
     }
 
     # Main function
     method main($args) {
         my $big_file = $self->_get_int($args, 0, 0);
-
-        for my $t (1 .. $self->THREADS_COUNT) {
-            push @threads, threads->create(sub { $self->_process($big_file); }, $t);
-        }
-
-        for my $t (@threads) {
-            $t->join();
-        }
+        $self->_process($self->target, $big_file);
     }
 
     # Test function
